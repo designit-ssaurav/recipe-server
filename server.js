2d6db9a069c8e2fd60f8649c51e5141185d3f381
saket saurav <saket.parasher@gmail.com>
2022-02-23 21:32:12 +0530

    server code and method based data controlled update

---
+++ server.js
@@ -0,0 +1,49 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const { v4: uuidv4 } = require("uuid");
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = new Date().toISOString();
+    req.body.lastEditedOn = new Date().toISOString();
+    req.body.updatedOn = new Date().toISOString();
+    req.body.status = "Assigned to timeline";
+    req.body.id = uuidv4();
+    console.log("STEPS :--- ", req.body.steps);
+    if (req.body.steps && req.body.steps.length > 0) {
+      req.body.steps = req.body.steps.map((step) => ({
+        ...step,
+        id: uuidv4(),
+      }));
+    }
+  }
+
+  if (req.method === "PUT" || req.method === "PATCH") {
+    req.body.lastEditedOn = new Date().toISOString();
+    req.body.updatedOn = new Date().toISOString();
+  }
+  next();
+});
+
+router.render = (req, res) => {
+  res.jsonp({
+    body: res.locals.data,
+  });
+};
+
+// Use default router
+server.use(router);
+server.listen(3001, () => {
+  console.log("\x1b[32m", "JSON Server Running on PORT 3001");
+  console.log("\x1b[32m", "--------------------------------");
+  console.log("\x1b[35m", "http://localhost:3001/recipe");
+});
